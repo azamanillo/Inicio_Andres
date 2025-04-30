56edc47735892597acfeaaaf009377062e606277
MI PC <MI PC@DESKTOP-L43VKR8>
2025-04-30 13:25:14 +0200

    modificacion almagro

--- PracticaInicial/src/git/Hola.java
+++ PracticaInicial/src/git/Hola.java
@@ -2,7 +2,7 @@ package git;
 
 public class Hola {
 		public static void main(String[] args) {
-			System.out.println("Hola");
+			System.out.println("Hola cro");
 		//  System.out.println("Miercoles");
 		}
 
